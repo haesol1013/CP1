3cb75288f6b5ffac20e4fd26a5f0d62384182aa0
shinhaesol <shinhaesol11@gmail.com>
2024-03-29 14:19:54 +0900

    change asgmt_3

--- week4/asgmt/week4_asgmt_3.py
+++ week4/asgmt/week4_asgmt_3.py
@@ -1,10 +1,6 @@
 def gcd(num1, num2):
-    s_num = num1 if num1 <= num2 else num2
-    cd = []
-    for i in range(1, s_num+1):
+    for i in range(1, min(num1, num2) + 1)[::1]:
         if num1 % i == 0 and num2 % i == 0:
-            cd.append(i)
+            cd = i
 
-    return cd[-1]
-
-# [::1], 유클리드 호제법
+    return cd
