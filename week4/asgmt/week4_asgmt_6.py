b63478d5101f3e59aed4c5aa05d90bd8da9a5fca
shinhaesol <shinhaesol11@gmail.com>
2024-03-29 14:25:45 +0900

    organize dir

--- week4/asgmt/week4_asgmt_6.py
+++
@@ -1,6 +0,0 @@
-def norm(lst):
-    result = 0
-    for i in lst:
-        result += i**2
-    result **= 0.5
-    return round(result, 2)
--- week4/asgmt/week4_asgmt_7.py
+++ week4/asgmt/week4_asgmt_7.py
@@ -1,9 +1,7 @@
-import math
-
-
-def softmax(a, b, c):
-    sigma = math.exp(a) + math.exp(b) + math.exp(c)
-    a_r = math.exp(a) / sigma
-    b_r = math.exp(b) / sigma
-    c_r = math.exp(c) / sigma
-    return a_r, b_r, c_r
+def fibo(n):
+    if n > 2:
+        return fibo(n-2) + fibo(n-1)
+    elif n == 2:
+        return 1
+    elif n == 1:
+        return 0
