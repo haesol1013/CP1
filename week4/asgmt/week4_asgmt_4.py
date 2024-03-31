9a2325664b68b2078aa08ff569c60112a3934888
shinhaesol <shinhaesol11@gmail.com>
2024-04-01 01:45:40 +0900

    in week4_asmgt_4 add 2 solutions

--- week4/asgmt/week4_asgmt_4.py
+++ week4/asgmt/week4_asgmt_4.py
@@ -1,8 +1,20 @@
-def count(string, char_):
+def count(string: str, target:str ) -> int:
     cnt = 0
-    
+
     for i in string:
-        if char_ == i:
+        if target == i:
             cnt += 1
 
     return cnt
+
+
+def count1(string: str, target: str) -> int:
+    return len([i for i in string if i == target])
+
+
+def count2(string: str, target: str) -> int:
+    return len(string) - len(string.replace(target, ''))
+
+
+print(count1("hello", 'e'))
+print(count2("hello", "e"))
