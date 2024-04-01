c15d1fdee2b946387f825ab2c116907a1bd8a1ff
shinhaesol <shinhaesol11@gmail.com>
2024-04-01 13:17:14 +0900

    week_asgmt_1_e

---
+++ week4/asgmt/week4_asgmt_1_e.py
@@ -0,0 +1,24 @@
+
+def mod(num1, num2):
+    opposite_sign = num1 * num2 < 0
+    need_inverse = num2 < 0
+
+    num1 = abs(num1)  # abs 함수 -> 넣은 숫자의 절댓값을 반환
+    num2 = abs(num2)
+    same_num = num1 == num2
+
+    while num1 >= num2:
+        num1 -= num2
+
+    if opposite_sign and not same_num:
+        num1 = num2 - num1
+
+    if need_inverse:
+        num1 *= -1
+
+    return num1
+
+
+a, b = map(int, input().split())
+print(mod(a, b))
+
