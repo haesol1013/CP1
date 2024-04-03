19318c6ec431ce4b1cab3eb4cf31d641a7485c35
shinhaesol <shinhaesol11@gmail.com>
2024-04-03 12:44:14 +0900

    edit week4_asgmt_8

--- week4/asgmt/week4_asgmt_8.py
+++ week4/asgmt/week4_asgmt_8.py
@@ -1,11 +1,13 @@
-import math
+from math import exp
 
 
-def softmax(a: int, b: int, c: int) -> tuple:
-    sigma = math.exp(a) + math.exp(b) + math.exp(c)
-    a_r = math.exp(a) / sigma
-    b_r = math.exp(b) / sigma
-    c_r = math.exp(c) / sigma
+def softmax(num1: int, num2: int, num3: int) -> tuple:
+    exp_num1, exp_num2, exp_num3 = exp(num1), exp(num2), exp(num3)
+
+    sigma = exp_num1 + exp_num2 + exp_num3
+    a_r = exp_num1 / sigma
+    b_r = exp_num2 / sigma
+    c_r = exp_num3 / sigma
     return a_r, b_r, c_r
 
 
