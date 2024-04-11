2079e1b0e29b1cc6f337304176e7763a12a1c65c
shinhaesol <shinhaesol11@gmail.com>
2024-04-11 22:48:22 +0900

    edit exam2

--- haesol/daiv_exam/exam02.py
+++ haesol/daiv_exam/exam02.py
@@ -3,13 +3,16 @@
 def pooling(matrix):
     while len(matrix[0]) > 1:
         new_matrix = []
+        length = len(matrix)
 
-        for row in range(0, len(matrix), 2):
+        for row in range(0, length, 2):
             tmp_row = []
 
-            for col in range(0, len(matrix), 2):
-                calc = sorted([matrix[row][col], matrix[row][col+1], matrix[row+1][col], matrix[row+1][col+1]])
+            for col in range(0, length, 2):
+                calc = sorted([matrix[row][col], matrix[row][col+1],
+                               matrix[row+1][col], matrix[row+1][col+1]])
                 tmp_row.append(calc[2])
+
             new_matrix.append(tmp_row)
 
         matrix = new_matrix[:]
