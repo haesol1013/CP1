3a2a683b149f4da4bc1ef4c384583597fb51f860
shinhaesol <shinhaesol11@gmail.com>
2024-04-17 01:54:50 +0900

    edit cnu_judge_0028.py

---
+++ haesol/cnu_judge/cnu_judge_0998.py
@@ -0,0 +1,34 @@
+def triangular_walk(walk: int) -> tuple:
+    i = 2
+    num = [1]
+    while num[-1] < walk:
+        num.append(num[-1] + i)
+        i += 1
+
+    pos = [0, 1]
+    pos_list = []
+    up = True
+
+    for i in range(0, len(num)):
+        if up:
+            pos[0] += 1
+            pos_list.append(tuple(pos))
+            for j in range(0, i):
+                pos[0] -= 1
+                pos[1] += 1
+                pos_list.append(tuple(pos))
+            up = False
+        else:
+            pos[1] += 1
+            pos_list.append(tuple(pos))
+            for j in range(0, i):
+                pos[0] += 1
+                pos[1] -= 1
+                pos_list.append(tuple(pos))
+            up = True
+
+    return pos_list[walk-1]
+
+
+n = int(input())
+print(triangular_walk(n))
