164dc7f9cbdf321fb65c4e6b0d612af6074e8330
Shin Haesol <111187655+haesol1013@users.noreply.github.com>
2024-05-23 16:12:09 +0900

    Update week11_asgmt_2.py

--- haesol/assignment/week11/week11_asgmt_2.py
+++ haesol/assignment/week11/week11_asgmt_2.py
@@ -3,11 +3,11 @@ import numpy as np
 
 data = np.load("Intel_Stock_History_from_1980_03_17.npy")
 
-data_str = map(str, data)
-first_digit = [int(x[0]) for x in data_str]
+str_data = map(str, data)
+first_digit = [int(x[0]) for x in str_data]
 cnt = np.bincount(first_digit)[1:]
 
-plt.bar(range(1, 10), cnt)
+plt.bar(range(1, 10), cnt / np.sum(cnt))
 
 plt.xticks(range(1, 10))
 
